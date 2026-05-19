ea7c0679ed060f1f4b9dc206e965d64880327d47
vardhanchittaluri1122 <161679554+vardhanchittaluri1122@users.noreply.github.com>
2026-05-19 16:47:11 +0530

    Add: baseball-game - submission-8

---
+++ Data Structures & Algorithms/baseball-game/submission-8.java
@@ -0,0 +1,86 @@
+class Solution {
+
+    int[] stack = new int[1000];
+
+    int top = -1;
+
+    // PUSH
+    public void push(int value) {
+
+        top++;
+
+        stack[top] = value;
+    }
+
+    // POP
+    public int pop() {
+
+        int removed = stack[top];
+
+        top--;
+
+        return removed;
+    }
+
+    // PEEK
+    public int peek() {
+
+        return stack[top];
+    }
+
+    // SIZE
+    public int size() {
+
+        return top + 1;
+    }
+
+    public int calPoints(String[] operations) {
+
+        int sum = 0;
+
+        for(int i = 0; i < operations.length; i++) {
+
+            if(operations[i].equals("+")) {
+
+                int first = pop();
+
+                int second = peek();
+
+                push(first);
+
+                int newScore = first + second;
+
+                push(newScore);
+
+                sum += newScore;
+            }
+
+            else if(operations[i].equals("D")) {
+
+                int newScore = peek() * 2;
+
+                push(newScore);
+
+                sum += newScore;
+            }
+
+            else if(operations[i].equals("C")) {
+
+                int removed = pop();
+
+                sum -= removed;
+            }
+
+            else {
+
+                int num = Integer.parseInt(operations[i]);
+
+                push(num);
+
+                sum += num;
+            }
+        }
+
+        return sum;
+    }
+}
