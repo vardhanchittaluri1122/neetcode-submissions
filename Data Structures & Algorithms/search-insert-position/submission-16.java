603de2ddeea33fc7d963149f26768e9925dfcfcc
vardhanchittaluri1122 <161679554+vardhanchittaluri1122@users.noreply.github.com>
2026-09-22 15:36:50 +0530

    Add: search-insert-position - submission-16

---
+++ Data Structures & Algorithms/search-insert-position/submission-16.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int res=0;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+                res=l;
+            }
+        }
+        return res;
+    }
+}
