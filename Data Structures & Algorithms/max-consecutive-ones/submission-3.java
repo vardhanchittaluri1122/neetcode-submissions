2ea48314a427121950bcfd9e6db84503931e730f
vardhanchittaluri1122 <161679554+vardhanchittaluri1122@users.noreply.github.com>
2026-09-23 17:19:50 +0530

    Add: max-consecutive-ones - submission-3

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0,l=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 1){
+                max=Math.max(max,i-l);
+                l=i+1;
+            }
+             if (i == nums.length - 1 && nums[i] == 1) {
+                max = Math.max(max, i - l + 1);
+            }
+        }
+        return max;
+    }
+}
